a54e11df2ecc73f15378d2b1a45bedc6b461f7f6
Zachary Cowan <cz8ch8ry@gmail.com>
2020-01-27 16:24:23 -0500

    Added maps

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -1,8 +1,34 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import PageTitle from '../components/PageTitle';
-import {PageRootContainer as RootContainer} from '../components/PageRootContainer';
+import { PageRootContainer as RootContainer } from '../components/PageRootContainer';
+
+// Maps
+import ciseFirstFloor from '../maps/ciseFirstFloor.png';
+import ciseSecondFloor from '../maps/ciseSecondFloor.png';
+import marstonBasement from '../maps/marstonBasement.png';
+import marstonSecondFloor from '../maps/marstonSecondFloor.png';
+
+// Maps list
+const maps = [
+  {
+    src: ciseFirstFloor,
+    label: 'CISE First Floor'
+  },
+  {
+    src: ciseSecondFloor,
+    label: 'CISE Second Floor'
+  },
+  {
+    src: marstonSecondFloor,
+    label: 'Marston Second Floor (Entrance Level)'
+  },
+  {
+    src: marstonBasement,
+    label: 'Marston First Floor (Basement Level)'
+  }
+];
 
 // Styled components
 const ContentContainer = styled.div`
@@ -12,8 +38,29 @@ const ContentContainer = styled.div`
   flex-direction: column;
 `;
 
-const Text = styled.p`
+const Section = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0px;
+  max-width: 680px;
+`;
+
+const LabelText = styled.h2`
+  font-family: Montserrat-Bold, Helvetica, sans-serif;
+  text-decoration: underline;
+`;
+
+const ContentLabel = styled.h4`
+  font-family: Montserrat-Bold, Helvetica, sans-serif;
+`;
+
+const ContentText = styled.div`
   font-family: Montserrat, Helvetica, sans-serif;
+`;
+
+const MapImage = styled.img`
+  width: 100%;
 `;
 
 const Event = () => {
@@ -21,7 +68,15 @@ const Event = () => {
     <RootContainer>
       <PageTitle title='Event' />
       <ContentContainer>
-        <Text>Event maps and more information coming soon.</Text>
+        <Section>
+          <LabelText>Maps</LabelText>
+          {maps.map(mp => (
+            <React.Fragment>
+              <ContentLabel>{mp.label}</ContentLabel>
+              <MapImage src={mp.src} />
+            </React.Fragment>
+          ))}
+        </Section>
       </ContentContainer>
     </RootContainer>
   );
